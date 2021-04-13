6a1275d90f19d6757e16e3d1f4380c44a7cb2cc6
MariusBudreika <mariusbudreika09@gmail.com>
2021-04-13 15:19:00 +0300

    a

--- reduce.js
+++ reduce.js
@@ -1,4 +1,4 @@
 const list = [10, 2, 8, 4, 6];
 
-const sum = list.reduce((totalSum, number) => totalSum + number, 0);
+const sum = list.reduce((totalSum, number) => totalSum + number, 0);  // 0 total sum pradine reiksme
 console.log(sum);
---
+++ sort.js
@@ -0,0 +1,6 @@
+const list = [10, 2, 8, 4, 6];
+const randomNumbers = [-10, 10, -8, 0, 8, -4, 4];
+
+const sortedList = list.sort();
+console.log(list);
+console.log(sortedList);
